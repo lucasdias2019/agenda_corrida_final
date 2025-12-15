055f2589f6cf4f908467d55022d2165d2c3a1761
Lucas Dias <ld.lucasdias15@gmail.com>
2025-12-15 08:15:14 -0300

    Correção de NameError em admin.py adicionando a importação dos modelos (ModeloDeCorrida, HistoricoCorrida)

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from .models import ModeloDeCorrida, HistoricoCorrida
 
 # modelos para que sejam visíveis e editáveis no /admin/
 admin.site.register(ModeloDeCorrida)
